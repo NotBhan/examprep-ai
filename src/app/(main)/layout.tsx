1fde170a9e560fb4d42ca87a2a3475563560e808
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-16 11:48:29 +0000

    infintely loading (_for element <MainLayout>_)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -59,7 +59,7 @@ export default function MainLayout({
 
   if (isSyllabusLoading || !mindMap) {
     return (
-      <div className="flex min-h-screen items-center justify-center">
+      <div className="flex min-h-screen w-full items-center justify-center">
         <div className="flex items-center space-x-4">
           <Skeleton className="h-12 w-12 rounded-full" />
           <div className="space-y-2">
