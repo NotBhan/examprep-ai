e98731b5a2bafd7413b79081c29ba4d0672971dd
Chandrabhan Mahato <humanpekka@gmail.com>
2025-09-16 17:40:25 +0000

    design fix: inconsisten margin for header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,7 +28,7 @@ export function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/95 px-4 backdrop-blur supports-[backdrop-filter]:bg-background/60 sm:static sm:h-auto sm:border-0 sm:bg-transparent md:px-6">
       <div className="flex items-center gap-2 md:hidden">
         <SidebarTrigger />
       </div>
